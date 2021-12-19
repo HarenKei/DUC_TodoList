7feee90edafe6b4d2a973aadcab5774a8ac50666
Seungmin Jeong <seungmin1266@gmail.com>
2021-12-19 22:41:16 +0900

    Hooks 사용예제

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App=()=> {
+
+  const [increase, setIncrease] = useState(1)
+
   return (
     <div className="App">
+      <h1>onClick Event Handling</h1>
+      {increase}
+      <button onClick={()=>setIncrease(increase+1)}>increase</button>
+      <button onClick={()=>setIncrease(increase-1)}>decrease</button>
       
     </div>
   );
